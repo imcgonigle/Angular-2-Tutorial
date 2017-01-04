67b58ce8b81bd86ed48ce6531e8a10a560fe0b15
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-01-04 12:37:23 -0600

    Add HeroService to component provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,7 +65,8 @@ import { HeroService } from './hero.service'
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+  providers: [HeroService]
 })
 export class AppComponent  { 
   title = 'Tour of Heroes';
