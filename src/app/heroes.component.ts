9d7010fe56f3437e999f355caf3f9a6af60e8c59
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-03-30 14:29:42 -0600

    update heroes component

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -6,7 +6,6 @@ import { HeroService } from './hero.service'
 @Component({
   selector: 'my-heroes',
   template: `
-    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes" 
@@ -69,7 +68,6 @@ import { HeroService } from './hero.service'
   providers: [HeroService]
 })
 export class HeroesComponent implements OnInit  { 
-  title = 'Tour of Heroes';
   selectedHero: Hero; 
   heroes: Hero[];
   
